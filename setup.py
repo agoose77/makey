9413d478ec13ceae5bdda83e3a41a0bf2dd56d3a
Angus Hollands <goosey15@gmail.com>
2018-12-15 13:34:44 +0000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='makey',
+    version='0.0.1',
+    packages=[''],
+    url='https://github.com/agoose77/makey',
+    license='MIT',
+    author='Angus Hollands',
+    author_email='goosey15@gmail.com',
+    description='Make libraries from url/tarball with CMAKE, and install using checkinstall.'
+)
