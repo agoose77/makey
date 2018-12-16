36f1801484839833e33c3df5b75fa3a2977394c4
Angus Hollands <goosey15@gmail.com>
2018-12-16 00:02:24 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='makey',
     version='0.0.1',
-    packages=[''],
+    packages=['makey'],
     url='https://github.com/agoose77/makey',
     license='MIT',
     author='Angus Hollands',
