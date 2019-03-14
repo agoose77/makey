c22afef6f355279fb4ee97673864329b95e8bc48
Angus Hollands <goosey15@gmail.com>
2019-03-14 08:51:42 +0000

    Bugfix

--- makey/__main__.py
+++ makey/__main__.py
@@ -10,7 +10,7 @@ HTTP_SCHEMES = {"http", "https"}
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("url_or_path", type=str, description="URL/local path of tar file, or url of git repositry prefixed with git+.")
+    parser.add_argument("url_or_path", type=str, help="URL/local path of tar file, or url of git repositry prefixed with git+.")
     args, unknown_args = parser.parse_known_args()
 
     # Parse file from URL or local tarball
